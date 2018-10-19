e5deef2bacf7fb68859c8a73f166a0316fa923d3
fgonza23 <fgonza0267@gmail.com>
2018-10-18 21:28:24 -0500

    added views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Places</h1>
+
+    <div>
+      Name: <input v-model="newPlace.name">
+      Address: <input v-model="newPlace.address">
+      <button @click="addPlace()">Add Place</button>
+    </div>
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  var axios = require('axios');
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      places: [],
+      newPlace: {name: "", address: ""}
+    };
+  },
+  created: function() {
+    axios
+    .get("http://localhost:3000/api/places")
+    .then(response => {
+      this.places = response.data;
+    });
+  },
+  methods: {
+    addPlace: function() {
+      var params = {
+                    name: this.newPlace.name,
+                    address: this.newPlace.address
+                    };
+    axios
+    .post("http://localhost:3000/api/places", params)
+
+    // axios
+    // .post("http://localhost:api/places", params)
+    // .then(function(response) {
+    //   this.places.push(response.data);
+    // }.bind(this));
+  },
+  computed: {}
+};
 </script>
